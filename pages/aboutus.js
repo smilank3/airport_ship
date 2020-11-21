2cd55ac724edaca92ba0e098801524413ed9924b
Brandon-Seda <42881128+Brandon-Seda@users.noreply.github.com>
2020-11-21 00:00:12 -0500

    Added to AboutUs Page

--- pages/aboutus.js
+++ pages/aboutus.js
@@ -9,7 +9,13 @@ About=()=>{
 	return (
 
 		<div>
-		<h1>This is about us page</h1>
+
+		<h1 style={{fontFamily: 'georgia', textAlign:"center" }}> Air Express </h1>
+			<p1 style={{fontFamily: 'georgia', textAlign:"justify" }} > Imagine you are traveling across the U.S and wanted to take along your favorite antique sword with you on your trip. 3 years ago this would not be possible without the 
+			headaches shipping beforehand and hoping your item arrives timely. Air Express was created with the vision that every person will be able to travel with ease wether it be going to the airport
+			and shipping directly to your destination without the hassel of TSA or even storing your items with us until you arrive back from your trip. Air Express has been sevicing customers in 
+			8 major airports and continually growing.   </p1>
+
 		</div>)
 }
 
